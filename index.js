3a8e89faa9cc39d4acce083ddfb4c8c4d8465cf9
amandasizo <68297460+amandasizo@users.noreply.github.com>
2024-03-07 13:31:05 -0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const survey = new Survey.Model(json);
-survey.applyTheme(themeJson);
-survey.onComplete.add((sender, options) => {
-    console.log(JSON.stringify(sender.data, null, 3));
-});
-
-$("#surveyElement").Survey({ model: survey });
